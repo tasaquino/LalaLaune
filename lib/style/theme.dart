10464cc8b9f4b44b7d4a90078ed14e62cc8806aa
tasaquino <thaisandrade@live.com>
2023-08-28 22:53:56 +0200

    putting together the styles from dict with app theme

---
+++ lib/style/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:lala_laune/style/style_dictionary.dart';
+
+var appTheme = ThemeData(
+  colorScheme: ColorScheme.fromSwatch()
+      .copyWith(primary: StyleDictionary.purple)
+      .copyWith(secondary: StyleDictionary.mustard)
+      .copyWith(background: Colors.white),
+  primaryColor: StyleDictionary.yellow,
+  hintColor: StyleDictionary.indigo,
+  cardColor: StyleDictionary.light_grey,
+  scaffoldBackgroundColor: StyleDictionary.white,
+  primaryTextTheme: const TextTheme(
+    bodySmall: StyleDictionary.body,
+    bodyMedium: StyleDictionary.body2,
+    headlineSmall: StyleDictionary.headline,
+    headlineMedium: StyleDictionary.headlineSemiBold,
+    headlineLarge: StyleDictionary.headline40,
+  ),
+);
