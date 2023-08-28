cd5b2ccb4a575753e35a443029f320fc3223cc5e
tasaquino <thaisandrade@live.com>
2023-08-28 22:53:25 +0200

    added support for l10n, supporting pt, en, fr, de and es

---
+++ lib/feature/authentication/presentation/login_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  String _errorMessage = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                AppLocalizations.of(context)!.welcomeText,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(height: 50),
+              OutlinedButton(
+                  onPressed: () async {
+                    // TODO: implement login button
+                  },
+                  child: Text(AppLocalizations.of(context)!.googleLogin)),
+              Text(
+                _errorMessage,
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+            ]),
+      ),
+    );
+  }
+}
